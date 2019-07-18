81438f3c769a262ce8242df1ff94503ecdccb732
E.B <edenbenbow@yahoo.com>
2019-07-18 02:55:07 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+  var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+  function addElementToBeginningOfArray() {
+chocolateBars.unshift("snickers");
+  console.log(chocolateBars);
+}
+
+addElementToBeginningOfArray(chocolateBars);
+
+function addElementToBeginningOfArray() {
+  var dogs = ["Sheltie", "Collie"];
+  dogs.push("Australian Shepherd");
+  console.log(dogs);
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var myArray = [one, two, three]
+    myArray[five] = five
+  console.log(myArray)
+}
+function destructivelyAddElementToBeginningOfArray(myArray)
